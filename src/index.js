15cfecbbacb73879668179c9f42afcd69a98af85
James Little <alphafoobar@users.noreply.github.com>
2021-10-20 21:31:51 +1300

    Style changes, look for existing trade ids

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-'use strict'
+'use strict';
 
-const { WSv2 } = require('bitfinex-api-node')
+const {WSv2} = require('bitfinex-api-node');
 
-let counter = 0
+let counter = 0;
+let trades = {};
 
-async function execute () {
-    const ws = new WSv2({transform: true})
-    ws.on('error', e => debug('WSv2 error: %s', e.message | e))
-    await ws.open()
+async function execute() {
+    const ws = new WSv2({transform: true});
+    ws.on('error', e => debug('WSv2 error: %s', e.message | e));
+    await ws.open();
 
-    ws.onTrades({ symbol: 'tBTCUSD' }, (t) => {
-        counter++
+    ws.onTrades({symbol: 'tBTCUSD'}, (t) => {
         const now = Date.now();
-        console.log(`${counter} BTC/USD trade: lag=${now-t.mts} amount:${t.amount} price:${t.price}`)
-    })
+        const id = '' + t.id;
+        if (trades[id]) {
+            console.log(`${counter} BTC/USD trade exists: lag=${now - t.mts} amount:${t.amount} price:${t.price}`)
+        } else {
+            counter++;
+            trades[id] = true;
+            console.log(`${counter} BTC/USD trade: lag=${now - t.mts}, Id: ${id}, Price: ${t.price}, Amount: ${t.amount}`)
+        }
+    });
 
-    await ws.subscribeTrades('tBTCUSD')
+    await ws.subscribeTrades('tBTCUSD');
     // await ws.close()
 }
 
